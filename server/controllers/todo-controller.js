958248d3a217b0350b9051bdd79a244d276e0fe8
Basim-B-1998 <basimbangalath8075@gmail.com>
2025-11-28 15:59:24 +0530

    add delete todo endpoint

--- server/controllers/todo-controller.js
+++ server/controllers/todo-controller.js
@@ -62,3 +62,26 @@ export const editTodo = async (req, res) => {
     });
   }
 }
+
+export const deleteTodo = async (req, res) => {
+  try{
+    const {id} = req.params;  
+    const todo = await Todo.findByIdAndDelete(id);
+    if(!todo){
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found"
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Todo deleted successfully"
+    });
+  }catch(e){
+    console.log(e);
+    res.status(500).json({  
+      success: false,
+      message: "Error occured"
+    });
+  }
+}
--- server/routes/todo-routes.js
+++ server/routes/todo-routes.js
@@ -2,11 +2,13 @@ import {createTodo} from "../controllers/todo-controller.js";
 import express from "express";
 import {fetchTodo} from "../controllers/todo-controller.js";
 import {editTodo} from "../controllers/todo-controller.js";
+import {deleteTodo} from "../controllers/todo-controller.js";
 
 const router = express.Router();
 
 router.post('/todo', createTodo);
 router.get('/todo', fetchTodo);
 router.put('/todo/:id', editTodo);
+router.delete('/todo/:id', deleteTodo);
 
 export default router;
