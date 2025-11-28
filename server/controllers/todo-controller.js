d2f3906b349b027b50dacec9f813adc9218e3545
Basim-B-1998 <basimbangalath8075@gmail.com>
2025-11-28 15:50:28 +0530

    add edit todo endpoint

--- server/controllers/todo-controller.js
+++ server/controllers/todo-controller.js
@@ -3,7 +3,7 @@ import Todo from '../models/Todo.js';
 
 export const createTodo = async (req, res) => { 
   try {
-    const { title, description } = req.body;
+    const { title, description,status } = req.body;
     const newlyCreatedTodo = new Todo({ title, description });
     await newlyCreatedTodo.save();
     res.status(201).json({
@@ -33,3 +33,32 @@ export const fetchTodo = async (req, res) => {
     });
   }
 };
+
+export const editTodo = async (req, res) => {
+  try{
+    const {id} = req.params;
+    const {title, description, status} = req.body;
+    const todo = await Todo.findById(id);
+    if(!todo){
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found"
+      });
+    }
+    todo.title = title;
+    todo.description = description;
+    todo.status = status;
+    await todo.save();
+    res.status(200).json({
+      success: true,
+      message: "Todo updated successfully",
+      data: todo
+    });
+  }catch(e){
+    console.log(e);
+    res.status(500).json({  
+      success: false,
+      message: "Error occured"
+    });
+  }
+}
--- server/routes/todo-routes.js
+++ server/routes/todo-routes.js
@@ -1,10 +1,12 @@
 import {createTodo} from "../controllers/todo-controller.js";
 import express from "express";
 import {fetchTodo} from "../controllers/todo-controller.js";
+import {editTodo} from "../controllers/todo-controller.js";
 
 const router = express.Router();
 
 router.post('/todo', createTodo);
 router.get('/todo', fetchTodo);
+router.put('/todo/:id', editTodo);
 
 export default router;
